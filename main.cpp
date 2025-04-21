d77a6e98139f2d7d89628bd33ac0a1d8a8eb1536
Phomolo Ntokoane <ntokoanephomolo76@gmail.com>
2025-04-21 15:47:45 +0200

    Refactor: Remove VSCode settings file; update shelves data format; enhance Book, Date, Floor, Manager, Record, Shelf, and Student classes with display functions and improved member functions

--- main.cpp
+++ main.cpp
@@ -3,10 +3,130 @@
 
 using namespace std;
 
+void FloorSystem(System&);
+void RecordSystem(System&);
+
 int main(int argc, char const *argv[])
 {
     System system;
-    system.loadData();
-    
+    // system.loadData();
+
+    int choice;
+    cout << "==========================Library system==================================" << "\n\n";
+
+    do
+    {
+        cout << "0. Stop Program" << endl;
+        cout << "1. Floors" << endl;
+        cout << "2. Records" << endl
+             << endl;
+        
+        cout << "Enter choice(0-2): ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 0:
+            cout << endl
+                 << "Terminating programm" << endl;
+            break;
+        case 1:
+            FloorSystem(system);
+            break;
+        case 2:
+            RecordSystem(system);
+            break;
+        default:
+            cout << "Error: please pick a choice between 0 and 2" << endl
+                 << endl;
+            break;
+        }
+    } while (choice != 0);
+
+    cout << "===========================================================================" << "\n\n";
     return 0;
+}
+
+void FloorSystem(System& system) {
+    cout << "=============================Floor system==================================" << "\n\n";
+    
+    int choice;
+    do
+    {
+        cout << "0. Exit Floor Sytstem" << '\n';
+        cout << "1. Display Floors" << '\n';
+        cout << "2. Display Shelves" << '\n';
+        cout << "3. Display Books in a shelf" << '\n';
+        cout << "4. Create Floor" << '\n';
+        cout << "5. Create shelf" << '\n';
+        cout << "6. Create book" << '\n';
+
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 0:
+            break;
+        case 1:
+            system.displayFloors();
+            break;
+        case 2:
+            system.displayShelves();
+            break;
+        case 3:
+            system.displayBooks();
+            break;
+        case 4:
+            system.createFloor();
+            break;
+        case 5:
+            system.createShelf();
+            break;
+        case 6:
+            system.createBook();
+            break;
+        default:
+            cout << "Error: please enter integer between 0 and 6"<< '\n';
+            break;
+        }
+    } while (choice != 0);  
+}
+
+void RecordSystem(System& system) {
+    cout << "=============================Record system=================================" << "\n\n";
+
+    int choice;
+    do
+    {
+        cout << "0. Exit Record System" << '\n';
+        cout << "1. Display All Records" << '\n';
+        cout << "2. Display Borrowed Books" << '\n';
+        cout << "3. Display Returned Books" << '\n';
+        cout << "4. Handle Book Return" << '\n';
+
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 0:
+            break;
+        case 1:
+            system.displayRecords();
+            break;
+        case 2:
+            system.displayBorrowedBooks();
+            break;
+        case 3:
+            system.displayReturnedBooks();
+            break;
+        case 4:
+            system.handleReturnBook();
+            break;
+        default:
+            cout << "Error: please enter an integer between 0 and 4" << '\n';
+            break;
+        }
+    } while (choice != 0);
 }
